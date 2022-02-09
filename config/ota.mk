329f47765bbf38b23977f461f21e1bc957517ed9
rezaadi0105 <rezaadipangestu5@gmail.com>
2022-02-09 22:08:26 +0700

    vendor: remove permissions ota
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -5,6 +5,3 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_PACKAGES += \
     Updates
-
-PRODUCT_COPY_FILES += \
-    vendor/ancient/config/permissions/com.ancient.ota.xml:system/etc/permissions/com.ancient.ota.xml
