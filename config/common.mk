e1d9e246b1a2aff90534e068f772b38ca3e4e33a
rezaadi0105 <rezaadipangestu5@gmail.com>
2022-02-26 16:07:00 +0700

    vendor: remove apex stuff
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -104,12 +104,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# Flatten APEXs for performance
-OVERRIDE_TARGET_FLATTEN_APEX := true
-# This needs to be specified explicitly to override ro.apex.updatable=true from
-# # prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
-PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
-
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
